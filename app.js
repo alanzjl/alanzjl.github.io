0aab6824dc8131527cf552e18be92b309062b8e3
Alan Zhao <alanzhao@csail.mit.edu>
2023-12-10 18:25:10 -0500

    change pic, fix jittering

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 window.onscroll = function() {scrollFunction()};
+var scolled = false;
+var lastScrollTop = 0;
 
 function scrollFunction() {
+  lastScrollTop = Math.max(document.body.scrollTop, document.documentElement.scrollTop, window.pageYOffset);
+  console.log(lastScrollTop);
+  if (Math.max(document.body.scrollTop, document.documentElement.scrollTop, window.pageYOffset, lastScrollTop) > 70) {
+    scolled = true;
+  } else if (Math.max(document.body.scrollTop, document.documentElement.scrollTop, window.pageYOffset, lastScrollTop) == 0){
+    scolled = false;
+  }
+  console.log(scolled, lastScrollTop);
   if (document.querySelector("html").clientWidth < 768) {
     document.getElementById("mainHeader").classList.add("shrink-header");
-    document.getElementById("profilePicture").style.width = "100%";
+    document.getElementById("profilePicture").classList.remove("shrink-picture");
     document.querySelector("#name h1").style.fontSize = "1.5rem";
-  } else if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  } else if (scolled) {
     document.getElementById("mainHeader").classList.add("shrink-header");
-    document.getElementById("profilePicture").style.width = "50%";
+    document.getElementById("profilePicture").classList.add("shrink-picture");
     document.querySelector("#name h1").style.fontSize = "1.5rem";
   } else {
     document.getElementById("mainHeader").classList.remove("shrink-header");
-    document.getElementById("profilePicture").style.width = "100%";
+    document.getElementById("profilePicture").classList.remove("shrink-picture");
     document.querySelector("#name h1").style.fontSize = "2.5rem";
   }
 }
