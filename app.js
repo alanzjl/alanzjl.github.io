78277e0ca8350e99a5d6ffdd77afcbd5a8ed1bca
Alan Zhao <alanzhao@csail.mit.edu>
2025-07-08 00:04:59 -0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 window.onscroll = function() {scrollFunction()};
 var scolled = false;
 var lastScrollTop = 0;
